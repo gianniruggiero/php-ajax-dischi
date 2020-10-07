02f04320236e586277e105730cbc1fafb8baf85c
GIanni Ruggiero <gianni@groovecompany.it>
2020-10-07 15:27:19 +0200

    files setup

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="dist/app.css">
+    <title>PHP aiax dischi</title>
+  </head>
+  <body>
+    <h1>TITOLO DELLA PAGINA DISCHI</h1>
+  </body>
+</html>
