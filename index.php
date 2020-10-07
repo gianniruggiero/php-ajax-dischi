ee9cd7ddad99d4a0acf854d5d4c5c95d986de59e
GIanni Ruggiero <gianni@groovecompany.it>
2020-10-07 17:11:55 +0200

    impostata pagina HTML

--- index.php
+++ index.php
@@ -6,10 +6,83 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
+    <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,300;0,400;0,700;0,900;1,700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="dist/app.css">
     <title>PHP aiax dischi</title>
   </head>
   <body>
-    <h1>TITOLO DELLA PAGINA DISCHI</h1>
+    <!-- header -->
+    <header>
+      <div class="header-wrap container">
+        <img class="logo-brand"src="img/logo.png" alt="">
+      </div>
+    </header>
+    <!-- /header -->
+    <!-- main -->
+    <main>
+      <div class="wrap-main container">
+        <div class="box-album">
+          <img src="https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg" alt="cover album">
+          <h2 class="title">NEW JERSEY</h2>
+          <h4 class="author">Bon Jovi</h4>
+          <h4 class="year">1988</h4>
+        </div>
+        <div class="box-album">
+          <img src="https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg" alt="cover album">
+          <h2 class="title">NEW JERSEY</h2>
+          <h4 class="author">Bon Jovi</h4>
+          <h4 class="year">1988</h4>
+        </div>
+        <div class="box-album">
+          <img src="https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg" alt="cover album">
+          <h2 class="title">TEN'S SUMMONER'S TALES</h2>
+          <h4 class="author">Bon Jovi</h4>
+          <h4 class="year">1988</h4>
+        </div>
+        <div class="box-album">
+          <img src="https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg" alt="cover album">
+          <h2 class="title">NEW JERSEY</h2>
+          <h4 class="author">Bon Jovi</h4>
+          <h4 class="year">1988</h4>
+        </div>
+        <div class="box-album">
+          <img src="https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg" alt="cover album">
+          <h2 class="title">NEW JERSEY</h2>
+          <h4 class="author">Bon Jovi</h4>
+          <h4 class="year">1988</h4>
+        </div>
+        <div class="box-album">
+          <img src="https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg" alt="cover album">
+          <h2 class="title">NEW JERSEY</h2>
+          <h4 class="author">Bon Jovi</h4>
+          <h4 class="year">1988</h4>
+        </div>
+        <div class="box-album">
+          <img src="https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg" alt="cover album">
+          <h2 class="title">NEW JERSEY</h2>
+          <h4 class="author">Bon Jovi</h4>
+          <h4 class="year">1988</h4>
+        </div>
+        <div class="box-album">
+          <img src="https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg" alt="cover album">
+          <h2 class="title">NEW JERSEY</h2>
+          <h4 class="author">Bon Jovi</h4>
+          <h4 class="year">1988</h4>
+        </div>
+        <div class="box-album">
+          <img src="https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg" alt="cover album">
+          <h2 class="title">NEW JERSEY</h2>
+          <h4 class="author">Bon Jovi</h4>
+          <h4 class="year">1988</h4>
+        </div>
+        <div class="box-album">
+          <img src="https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg" alt="cover album">
+          <h2 class="title">NEW JERSEY</h2>
+          <h4 class="author">Bon Jovi</h4>
+          <h4 class="year">1988</h4>
+        </div>
+      </div>
+    </main>
+    <!-- /main -->
   </body>
 </html>
