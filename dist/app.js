346c790b3aad8231545da27c07cbf2b096464870
GIanni Ruggiero <gianni@groovecompany.it>
2020-10-08 15:26:55 +0200

    M2: aggiunti document ready,  function renderAlbum(), selettori jQuery

--- dist/app.js
+++ dist/app.js
@@ -16091,29 +16091,42 @@ var $ = __webpack_require__(/*! jquery */ "./node_modules/jquery/dist/jquery.js"
 
 var Handlebars = __webpack_require__(/*! handlebars */ "./node_modules/handlebars/dist/cjs/handlebars.js");
 
-var source = document.getElementById("album-template").innerHTML;
-var template = Handlebars.compile(source);
-$.ajax({
-  url: 'http://localhost/php-ajax-dischi/api.php',
-  method: "GET",
-  success: function success(data) {
-    for (var i = 0; i < data.length; i++) {
-      var context = {
-        url: data[i].poster,
-        title: data[i].title,
-        author: data[i].author,
-        year: data[i].year
-      };
-      var html = template(context);
-      $(".wrap-main").append(html);
+$(document).ready(function () {
+  // chiamata ajax per recuperare dati del db
+  $.ajax({
+    url: 'http://localhost/php-ajax-dischi/api.php',
+    method: "GET",
+    success: function success(data) {
+      renderAlbum(data);
+    },
+    error: function error(richiesta, stato, errori) {
+      alert("E' avvenuto un errore. " + errori);
     }
+  });
+}); // funzione che inietta i dati del DB nel template handlebars e stampa nel DOM
 
-    ;
-  },
-  error: function error(richiesta, stato, errori) {
-    alert("E' avvenuto un errore. " + errori);
+function renderAlbum(albums) {
+  var source = $("#album-template").html();
+  var template = Handlebars.compile(source); // ciclo per caricare il template con i dati del DB
+
+  for (var i = 0; i < albums.length; i++) {
+    // manipola il context con i dati del DB
+    var context = {
+      url: albums[i].poster,
+      title: albums[i].title,
+      author: albums[i].author,
+      year: albums[i].year
+    }; // crea il codice HTML, compilando il template con il context manipolato con id ait del DB
+
+    var html = template(context); // inietta nel DOM il codice Html, appendendolo al div elenco album
+
+    $(".wrap-main").append(html);
   }
-});
+
+  ;
+}
+
+;
 
 /***/ }),
 
--- src/app.js
+++ src/app.js
@@ -1,28 +1,38 @@
 const $ = require('jquery');
 const Handlebars = require('handlebars');
 
-var source = document.getElementById("album-template").innerHTML;
-var template = Handlebars.compile(source);
-
-
-$.ajax(
-  {
-    url: 'http://localhost/php-ajax-dischi/api.php',
-    method: "GET",
-    success: function (data) {
-      for (var i = 0; i < data.length; i++) {
-        var context = {
-          url: data[i].poster,
-          title: data[i].title,
-          author: data[i].author,
-          year: data[i].year,
-        };
-        var html = template(context);
-        $(".wrap-main").append(html);
-      };
-    },
-    error: function (richiesta, stato, errori) {
-      alert("E' avvenuto un errore. " + errori);
+$(document).ready(function() {
+  // chiamata ajax per recuperare dati del db
+  $.ajax(
+    {
+      url: 'http://localhost/php-ajax-dischi/api.php',
+      method: "GET",
+      success: function (data) {
+        renderAlbum(data);
+      },
+      error: function (richiesta, stato, errori) {
+        alert("E' avvenuto un errore. " + errori);
+      }
     }
-  }
-);
+  );
+});
+
+// funzione che inietta i dati del DB nel template handlebars e stampa nel DOM
+function renderAlbum(albums) {
+  var source = $("#album-template").html();
+  var template = Handlebars.compile(source);
+  // ciclo per caricare il template con i dati del DB
+  for (var i = 0; i < albums.length; i++) {
+    // manipola il context con i dati del DB
+    var context = {
+      url: albums[i].poster,
+      title: albums[i].title,
+      author: albums[i].author,
+      year: albums[i].year,
+    };
+    // crea il codice HTML, compilando il template con il context manipolato con id ait del DB
+    var html = template(context);
+    // inietta nel DOM il codice Html, appendendolo al div elenco album
+    $(".wrap-main").append(html);
+  };
+};
